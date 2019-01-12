b55ddc2448ee35c8d82ad8844af185f3f75c2e61
Murilo Trigo <murilotrigopaz@gmail.com>
2019-01-12 03:02:26 -0500

    Implements tile selection for the four one-sized test

---
+++ Assets/Scripts/SelectionScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SelectionScript : MonoBehaviour
+{
+    /* Inspector */
+
+    /* Life Cycle */
+
+    private void OnMouseDown()
+    {
+        GetComponentInParent<GridScript>().SetSelectedTile( gameObject );
+    }
+
+    /* Public */
+
+    /* Private */
+}
