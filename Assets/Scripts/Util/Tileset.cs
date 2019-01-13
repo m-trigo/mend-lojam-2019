23d24dca6293d0622fdc87128d6dc64a36e3b1a8
fenlys <fenlys79@gmail.com>
2019-01-12 22:43:42 -0500

    adds file parsing mechanism for levels; tile creation for now is hardcoded for 0th coordinate (for 1-sized tiles)

---
+++ Assets/Scripts/Util/Tileset.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Tileset
+{
+
+    public Tileset()
+    {
+        Dictionary<char, Tile> tiles = new Dictionary<char, Tile>();
+        tiles.Add('A', new Tile());
+        tiles.Add('B', new Tile());
+        tiles.Add('C', new Tile());
+        tiles.Add('D', new Tile());
+        Tiles = tiles;
+    }
+
+    public Dictionary<char, Tile> Tiles { get; set; }
+}
